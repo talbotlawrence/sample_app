42375f6e72339fc5f8277c85b84714bc4c2c7c09
talbot <talbotlawrence75@gmail.com>
2011-05-12 21:38:12 -0500

    finished layout and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
-   @title = "Home"
+   @title = "Ruby Sample App | Home"
   end
 
   def contact
-   @title = "Contact"
+   @title = "Ruby Sample App | Contact"
   end
 
   def about
-   @title = "About"
+   @title = "Ruby Sample App | About"
+  end
+
+  def help
+   @title = "Help"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+ get "users/new"
 
-  get "pages/contact"
+ get "pages/home"
+ get "pages/contact"
+ get "pages/about"
 
-  get "pages/about"
+ match '/signup', :to => 'users#new'
+ match '/contact', :to => 'pages#contact'
+ match '/about', :to => 'pages#about'
+ match '/help', :to => 'pages#help'
+
+ root :to => "pages#home"
+  # root :to => "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
